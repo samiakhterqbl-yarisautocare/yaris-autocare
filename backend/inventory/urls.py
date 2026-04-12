30658b08015b0cc8c9e2a7374b879bf81c887887
samiakhterqbl-yarisautocare <samiakhterqbl@gmail.com>
2026-04-12 20:13:07 +1000

    Update urls.py

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BulkPartCreateView, get_car_details
+from .views import BulkPartCreateView, get_car_details, PartCreateView # Make sure to import your single create view
 
 urlpatterns = [
+    # This matches the /api/parts/ call from your React code
+    path('parts/', PartCreateView.as_view(), name='create-part'), 
+    
     path('bulk-create/', BulkPartCreateView.as_view(), name='bulk-create-parts'),
     path('car-details/<int:car_id>/', get_car_details, name='car-details'),
 ]
