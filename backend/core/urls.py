333bc049528ecfe39f8d5dcf4f8ce72fbc741ca9
yaris_autocare <yarisautcare@gmail.com>
2026-04-12 11:59:22 +1000

    Confirmed local server working - fixing admin path

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,18 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
-    path('admin/', admin.h.urls), # THIS LINE MUST BE HERE
-
     path('admin/', admin.site.urls),
-
-    path('api/', include('inventory.urls')), # This connects your car parts logic
+    path('api/', include('inventory.urls')),
 ]
 
-# This helps show images while you are developing locally
 if settings.DEBUG:
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
