4bf1bc5081634e5ab9e47219c8ad562d950b288c
yaris_autocare <yarisautcare@gmail.com>
2026-04-12 11:53:00 +1000

    Fix typo in admin urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,10 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.h.urls), # THIS LINE MUST BE HERE
+
+    path('admin/', admin.site.urls),
+
     path('api/', include('inventory.urls')), # This connects your car parts logic
 ]
 
 # This helps show images while you are developing locally
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
