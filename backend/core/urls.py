cf3ffd7ce313e86471a3f4149e58d412d56d9d8a
samiakhterqbl-yarisautocare <samiakhterqbl@gmail.com>
2026-04-12 11:43:33 +1000

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/inventory/', include('inventory.urls')),
+    path('admin/', admin.h.urls), # THIS LINE MUST BE HERE
+    path('api/', include('inventory.urls')), # This connects your car parts logic
 ]
+
+# This helps show images while you are developing locally
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
